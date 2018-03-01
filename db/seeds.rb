de6b5a7bb5f6e9f8ed627b40d29cd7db75290012
Dipak <dade.dipakg@gmail.com>
2018-03-01 13:34:12 +0530

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(full_name: "Victor Young", email: "victor@progress_app.com", password: "password123", password_confirmation: "password123")
+
+["Open", "In Progress", "Pending", "On Hold", "Completed", "Closed"].each do |status|
+  Status.find_or_create_by(name: status)
+end

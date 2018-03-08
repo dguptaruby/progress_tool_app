cac2188240205185e81e58af375b7ca4d4c2e3cb
Dipak <dade.dipakg@gmail.com>
2018-03-08 22:45:43 +0530

    Minor changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const merge = require('webpack-merge')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.append('typescript', typescript)
-// module.exports = environment
-
-// const { environment } = require('@Rails/webpacker')
-
-/*environment.loaders.set('html', {
-  test: /\.html$/,
-  exclude: /node_modules/,
-  loaders: ['html-loader'] 
-})*/
 
 environment.loaders.append('html', {
   test: /\.html$/,
